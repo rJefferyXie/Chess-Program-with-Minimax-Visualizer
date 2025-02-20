8e6522e47a05d462ae479ef114e28a352521670f
Jeffery Xie <jeffxie27@outlook.com>
2025-02-20 15:37:33 -0600

    Refactor for maintainability.

--- game/material.py
+++ game/material.py
@@ -18,199 +18,76 @@ class Material(object):
     self.captured_black_pieces = []
     self.captured_white_pieces = []
 
+  def get_image(self, piece, color_index):
+    image_map = {
+      Pawn: pawns[color_index],
+      Knight: knights[color_index],
+      Bishop: bishops[color_index],
+      Rook: rooks[color_index],
+      Queen: queens[color_index]
+    }
+    image = image_map.get(type(piece))
+    return pygame.transform.scale(image, (32, 32)) if image else None
+
   def draw_captured(self, window, color):
-    # Draw captured black pieces
-    for piece in range(len(self.captured_black_pieces)):
+    positions = {
+      "White": [(480, 410, 25, 0), (480, 435, 25, -8)],
+      "Black": [(480, 35, 25, 0), (480, 55, 25, -8)]
+    }
+    color_index = 1  # Black pieces
+    for idx, piece in enumerate(self.captured_black_pieces):
+      image = self.get_image(piece, color_index)
+      if image:
+        x_offset, y_base, spacing, shift = positions[color][0 if idx < 8 else 1]
+        window.blit(image, (x_offset + (idx + shift) * spacing, y_base))
 
-      if isinstance(self.captured_black_pieces[piece], Pawn):
-        image = pygame.transform.scale(pawns[1], (32, 32))
-
-      elif isinstance(self.captured_black_pieces[piece], Knight):
-        image = pygame.transform.scale(knights[1], (32, 32))
-
-      elif isinstance(self.captured_black_pieces[piece], Bishop):
-        image = pygame.transform.scale(bishops[1], (32, 32))
-
-      elif isinstance(self.captured_black_pieces[piece], Rook):
-        image = pygame.transform.scale(rooks[1], (32, 32))
-
-      else:
-        image = pygame.transform.scale(queens[1], (32, 32))
-
-      if color == "White":
-        if piece < 8:
-          window.blit(image, (480 + piece * 25, 410))
-        else:
-          window.blit(image, (480 + (piece - 8) * 25, 435))
-      else:
-        if piece < 8:
-          window.blit(image, (480 + piece * 25, 35))
-        else:
-          window.blit(image, (480 + (piece - 8) * 25, 55))
-
-    # Draw captured white pieces
-    for piece in range(len(self.captured_white_pieces)):
-      if isinstance(self.captured_white_pieces[piece], Pawn):
-        image = pygame.transform.scale(pawns[0], (32, 32))
-
-      elif isinstance(self.captured_white_pieces[piece], Knight):
-        image = pygame.transform.scale(knights[0], (32, 32))
-
-      elif isinstance(self.captured_white_pieces[piece], Bishop):
-        image = pygame.transform.scale(bishops[0], (32, 32))
-
-      elif isinstance(self.captured_white_pieces[piece], Rook):
-        image = pygame.transform.scale(rooks[0], (32, 32))
-
-      else:
-        image = pygame.transform.scale(queens[0], (32, 32))
-
-      if color == "White":
-        if piece < 8:
-          window.blit(image, (480 + piece * 25, 25))
-        else:
-          window.blit(image, (480 + (piece - 8) * 25, 45))
-      else:
-        if piece < 8:
-          window.blit(image, (480 + piece * 25, 400))
-        else:
-          window.blit(image, (480 + (piece - 8) * 25, 425))
+    color_index = 0  # White pieces
+    for idx, piece in enumerate(self.captured_white_pieces):
+      image = self.get_image(piece, color_index)
+      if image:
+        x_offset, y_base, spacing, shift = positions[color][0 if idx < 8 else 1]
+        window.blit(image, (x_offset + (idx + shift) * spacing,
+                    y_base - 385 if color == "White" else 375))
 
   def draw_advantages(self, window, color):
+    def draw_text(pieces_list, advantage, y_offsets):
+      text = my_font.render(f"+{advantage}", True, (0, 0, 0))
+      pieces_list.append(text)
+      for piece in range(len(pieces_list)):
+        if not isinstance(pieces_list[piece], (Pawn, Knight, Bishop, Rook, Queen)):
+          x_offset = 460 + (piece + 1) * 25
+          y_offset = y_offsets[0] if piece <= 8 else y_offsets[1]
+          window.blit(pieces_list[piece], (x_offset, y_offset))
+          pieces_list.pop(-1)
+
     if self.white_advantage > self.black_advantage:
-      text = my_font.render("+" + str(self.white_advantage), True, (0, 0, 0))
-      self.captured_black_pieces.append(text)
-
-      for piece in range(len(self.captured_black_pieces)):
-        if not isinstance(self.captured_black_pieces[piece], (Pawn, Knight, Bishop, Rook, Queen)):
-
-          if color == "White":
-            if piece <= 8:
-              window.blit(
-                self.captured_black_pieces[piece], (460 + (piece + 1) * 25, 420))
-            else:
-              window.blit(
-                self.captured_black_pieces[piece], (460 + (piece - 7) * 25, 445))
-
-          else:
-            if piece <= 8:
-              window.blit(
-                self.captured_black_pieces[piece], (460 + (piece + 1) * 25, 45))
-            else:
-              window.blit(
-                self.captured_black_pieces[piece], (460 + (piece - 7) * 25, 70))
-
-          self.captured_black_pieces.pop(-1)
-
+      draw_text(self.captured_black_pieces, self.white_advantage,
+                (420 if color == "White" else 45, 445 if color == "White" else 70))
     elif self.black_advantage > self.white_advantage:
-      text = my_font.render("+" + str(self.black_advantage), True, (0, 0, 0))
-      self.captured_white_pieces.append(text)
-
-      for piece in range(len(self.captured_white_pieces)):
-        if not isinstance(self.captured_white_pieces[piece], (Pawn, Knight, Bishop, Rook, Queen)):
-
-          if color == "White":
-            if piece <= 8:
-              window.blit(
-                self.captured_white_pieces[piece], (460 + (piece + 1) * 25, 35))
-            else:
-              window.blit(
-                self.captured_white_pieces[piece], (460 + (piece - 7) * 25, 60))
-
-          else:
-            if piece <= 8:
-              window.blit(
-                self.captured_white_pieces[piece], (460 + (piece + 1) * 25, 410))
-            else:
-              window.blit(
-                self.captured_white_pieces[piece], (460 + (piece - 7) * 25, 435))
-
-          self.captured_white_pieces.pop(-1)
+      draw_text(self.captured_white_pieces, self.black_advantage,
+                (35 if color == "White" else 410, 60 if color == "White" else 435))
 
   def update_advantages(self, board):
-    black_adv = 0
-    white_adv = 0
+    value_map = {Pawn: 1, Knight: 3, Bishop: 3, Rook: 4, Queen: 9}
+    black_adv, white_adv = 0, 0
 
     for row in board.board:
       for piece in row:
-        if isinstance(piece, (Pawn, Knight, Bishop, Rook, Queen, King)):
+        if type(piece) in value_map:
           if piece.color == "White":
-            if isinstance(piece, Pawn):
-              white_adv += 1
-
-            elif isinstance(piece, Knight) or isinstance(piece, Bishop):
-              white_adv += 3
-
-            elif isinstance(piece, Rook):
-              white_adv += 4
-
-            elif isinstance(piece, Queen):
-              white_adv += 9
-
+            white_adv += value_map[type(piece)]
           else:
-            if isinstance(piece, Pawn):
-              black_adv += 1
+            black_adv += value_map[type(piece)]
 
-            elif isinstance(piece, Knight) or isinstance(piece, Bishop):
-              black_adv += 3
-
-            elif isinstance(piece, Rook):
-              black_adv += 4
-
-            elif isinstance(piece, Queen):
-              black_adv += 9
-
-    if white_adv > black_adv:
-      self.white_advantage = white_adv - black_adv
-      self.black_advantage = 0
-
-    elif black_adv > white_adv:
-      self.black_advantage = black_adv - white_adv
-      self.white_advantage = 0
-
-    else:
-      self.white_advantage = 0
-      self.black_advantage = 0
+    self.white_advantage = max(0, white_adv - black_adv)
+    self.black_advantage = max(0, black_adv - white_adv)
 
   def add_to_captured_pieces(self, piece, capture_list):
-    # Insert pawns to start of list
-    if isinstance(piece, Pawn):
-      capture_list.insert(0, piece)
+    priority = [Pawn, Knight, Bishop, Rook, Queen]
+    piece_priority = priority.index(type(piece))
 
-    # Insert knights at the start of list, or after last pawn
-    elif isinstance(piece, Knight):
-      if len(capture_list) == 0 or not isinstance(capture_list[0], Pawn):
-        capture_list.insert(0, piece)
-      else:
-        for i in range(len(capture_list)):
-          curr = capture_list[i]
-          if not isinstance(curr, Pawn):
-            capture_list.insert(i, piece)
-            break
-          if i == len(capture_list) - 1:
-            capture_list.insert(i + 1, piece)
-
-    # Insert bishops at the start of the list, or after last pawn / knight
-    elif isinstance(piece, Bishop):
-      if len(capture_list) == 0 or (not isinstance(capture_list[0], Pawn)
-                                    and not isinstance(capture_list[0], Knight)):
-        capture_list.insert(0, piece)
-      else:
-        for i in range(len(capture_list)):
-          curr = capture_list[i]
-          if not isinstance(curr, Pawn) and not isinstance(curr, Knight):
-            capture_list.insert(i, piece)
-            break
-          if i == len(capture_list) - 1:
-            capture_list.insert(i + 1, piece)
-
-    # Insert rooks at the end of the list, or after before queen
-    elif isinstance(piece, Rook):
-      if len(capture_list) == 0 or not isinstance(capture_list[-1], Queen):
-        capture_list.append(piece)
-      else:
-        capture_list.insert(len(capture_list) - 1, piece)
-
-    # Insert queen at the end of the list
-    elif isinstance(piece, Queen):
-      capture_list.append(piece)
+    for i, curr in enumerate(capture_list):
+      if priority.index(type(curr)) > piece_priority:
+        capture_list.insert(i, piece)
+        return
+    capture_list.append(piece)
