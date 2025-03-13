5ac439226f64fe35d617f6a7039425d98d250fa5
JEFFERY XIE <jeffxie27@outlook.com>
2025-03-13 14:28:11 -0600

    Now prints total time spent to make move, and moves evaluated per second.

--- game/profiler.py
+++ game/profiler.py
@@ -15,6 +15,9 @@ class Profiler():
       result = func(self,*args, **kwargs)
       elapsed_time = time.time() - start_time
 
+      if not self.profiler.start_time:
+        self.profiler.start_time = start_time
+
       self.profiler.profile_data[func.__name__]["total_time"] += elapsed_time
       self.profiler.profile_data[func.__name__]["call_count"] += 1
 
@@ -22,17 +25,21 @@ class Profiler():
 
     return wrapper
 
-  def print_profile_summary(self):
+  def print_profile_summary(self, moves_evaluated):
     print(f"\n{'-' * 10} Chess Engine Profiling Summary {'-' * 10}")
+    print(f"Total time to calculate move: {(time.time() - self.start_time):.2f}s.")
+    print(f"Moves evaluated per second: {int(moves_evaluated // (time.time() - self.start_time))}")
+    print("-" * 52)
+
     print("{:<20} {:<15} {:<15}".format("Function Name", "Call Count", "Total Time (s)"))
     print("-" * 52)
 
     for func_name, data in self.profile_data.items():
       print("{:<20} {:<15} {:<15.3f}".format(func_name, data["call_count"], data["total_time"]))
 
-    print("-" * 52)
-
   def reset_profiler(self):
+    self.start_time = None
+
     def default_profiling_data():
       return {"total_time": 0, "call_count": 0}
 
